b7e8d4eb08c0d309721f730955f72678a0d20a63
SKO-hub <minervakavene@gmail.com>
2022-10-25 15:32:51 +0100

    Added a reptile and amphibians class

---
+++ Animals/src/Amphibian.java
@@ -0,0 +1,11 @@
+public class Amphibian extends Animal{
+    private String environment;
+
+    public Amphibian(String environment) {
+        this.environment = environment;
+    }
+
+
+    public String getEnvironment() { return environment; }
+    public void setEnvironment(String environment) { this.environment = environment; }
+}
---
+++ Animals/src/Reptile.java
@@ -0,0 +1,11 @@
+public class Reptile extends Animal{
+    private String environment;
+
+    public Reptile(String environment) {
+        this.environment = environment;
+    }
+
+
+    public String getEnvironment() { return environment; }
+    public void setEnvironment(String environment) { this.environment = environment; }
+}
